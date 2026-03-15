e1ba9191e56dc6d309e3efc60e0008f234350e43
George Riabov <george.riabov3105@gmail.com>
2026-03-15 15:58:55 +0100

    finish hw2

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -16,3 +16,37 @@ export const getNoteById = async (req, res) => {
   }
   res.status(200).json(note);
 };
+// POST /note
+export const createNote = async (req, res) => {
+  const note = await Note.create(req.body);
+  res.status(201).json(note);
+};
+
+// DELETE /note
+export const deleteNote = async (req, res) => {
+  const { noteId } = req.params;
+  const note = await Note.findOneAndDelete({
+    _id: noteId,
+  });
+
+  if (!note) {
+    throw createHttpError(404, 'Note not found');
+  }
+
+  res.status(200).json(note);
+};
+
+// PATCH /note
+export const updateNote = async (req, res) => {
+  const { noteId } = req.params;
+
+  const note = await Note.findOneAndUpdate({ _id: noteId }, req.body, {
+    new: true,
+  });
+
+  if (!note) {
+    throw createHttpError(404, 'Note not found');
+  }
+
+  res.status(200).json(note);
+};
--- src/middleware/notFoundHandler.js
+++ src/middleware/notFoundHandler.js
@@ -1,4 +1,4 @@
 // 404
-export const notFoundHandler ((req, res) => {
+export const notFoundHandler = (req, res) => {
   res.status(404).json({ message: 'Route not found' });
-});
+};
