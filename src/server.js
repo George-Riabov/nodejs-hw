baa0dd6902655a393d2a2c069d149594a381444b
George Riabov <george.riabov3105@gmail.com>
2026-03-15 14:07:07 +0100

    finish first half

--- src/server.js
+++ src/server.js
@@ -1,46 +1,21 @@
 import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
-import pinoHttp from 'pino-http';
+import { connectMongoDB } from './db/connectMongoDB.js';
+import notesRoutes from './routes/notesRoutes.js';
 import 'dotenv/config';
+import { logger } from './middleware/logger.js';
 
 const app = express();
 const PORT = Number(process.env.PORT) || 3000;
 
+app.use(logger);
 app.use(cors());
-app.use(pinoHttp());
 app.use(helmet());
 app.use(express.json());
+app.use(notesRoutes);
 
-// GET /notes
-app.get('/notes', (req, res) => {
-  res.status(200).json({ message: 'Retrieved all notes' });
-});
-
-// GET /notes/noteId
-app.get('/notes/:noteId', (req, res) => {
-  res
-    .status(200)
-    .json({ message: `Retrieved note with ID: ${req.params.noteId}` });
-});
-
-app.get('/test-error', (req, res) => {
-  throw new Error('Simulated server error');
-});
-
-// 404
-app.use((req, res) => {
-  res.status(404).json({ message: 'Route not found' });
-});
-
-// error middleware
-app.use((err, req, res, next) => {
-  const isProd = process.env.NODE_ENV === 'production';
-
-  res.status(500).json({
-    message: isProd ? 'Something went wrong' : err.message,
-  });
-});
+await connectMongoDB();
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
