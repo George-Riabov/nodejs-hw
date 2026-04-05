f65934834bebb4df87431a41823fbc9d2186ef61
George Riabov <george.riabov3105@gmail.com>
2026-04-05 17:50:26 +0200

    finish hw

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -5,11 +5,16 @@ import {
   logoutUser,
   registerUser,
   refreshUserSession,
+  requestResetEmailSchema,
 } from '../controllers/authController.js';
 import {
   loginUserSchema,
   registerUserSchema,
+  requestResetEmail,
 } from '../validations/authValidation.js';
+
+import { resetPassword } from '../controllers/authController.js';
+import { resetPasswordSchema } from '../validations/authValidation.js';
 
 const router = Router();
 
@@ -17,5 +22,15 @@ router.post('/auth/register', celebrate(registerUserSchema), registerUser);
 router.post('/auth/login', celebrate(loginUserSchema), loginUser);
 router.post('/auth/logout', logoutUser);
 router.post('/auth/refresh', refreshUserSession);
+router.post(
+  '/auth/request-reset-email',
+  celebrate(requestResetEmailSchema),
+  requestResetEmail,
+);
+router.post(
+  '/auth/reset-password',
+  celebrate(resetPasswordSchema),
+  resetPassword,
+);
 
 export default router;
